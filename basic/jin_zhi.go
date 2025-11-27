6f6c56b59e076e9e5f1870c9c96a2e2ab86e4ca0
zqgu5 <zqgu5@iflytek.com>
2025-11-27 21:44:04 +0800

    new file:   basic/jin_zhi.go 	modified:   basic/primitive.go

---
+++ basic/jin_zhi.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/*
+多行
+
+注释
+这种很少见
+*/
+
+// excel最后一列是XFD，求excel有多少列
+func main() {
+	fmt.Printf("A=%d Z=%d\n", 'A', 'Z')
+	var base int = 'Z' - 'A' + 1
+	fmt.Println("进制", base)
+	var total int
+	total += 'D' - 'A' + 1
+	total += ('F' - 'A' + 1) * base
+	total += ('X' - 'A' + 1) * base * base
+	fmt.Println("总列数", total)
+
+}
--- basic/primitive.go
+++ basic/primitive.go
@@ -2,8 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
+func main2() {
 	var MyName int
 	fmt.Println(MyName)
+	var age int = 30
+	fmt.Println(age)
+	var b = age // 自动类型推导
+	_ = b
+	c := b + 1 //第一次出现 短声明 a:= 等同于 var a =
+	age = c    //第二次出现不能有声明符号
+	fmt.Println(age)
+
+	number := 1_000_000 // 数字字面量中的下划线
+	fmt.Println(number)
+
+	m := 13.13 // 浮点型, := 默认是float64
+	fmt.Println(m)
+
+	var isOk bool = true // 布尔型, 默认值false
+	fmt.Println(isOk)
+
+	var (
+		d uint8 // 别名type
+		e int8
+		f float32
+		g float64
+	)
+	_, _, _, _ = d, e, f, g
 
 }
