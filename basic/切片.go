06479771faa54efd948dbfe3d5cdfe73e5c3472d
zqgu5 <zqgu5@iflytek.com>
2025-12-01 21:59:48 +0800

    feat: 新增字符串，完善切片

--- basic/切片.go
+++ basic/切片.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func SliceInit() {
 
@@ -57,7 +60,137 @@ func Expansion() {
 	}
 }
 
-func main() {
+// 内存共享
+func MemoryShare() {
+	s := make([]int, 3, 3)
+	s[0], s[1], s[2] = 1, 2, 3
+	fmt.Println(s)
+	s1 := s
+	// 修改s1会影响s
+	s1[0] = 100
+	fmt.Println("s:", s)
+	fmt.Println("s1:", s1)
+
+	// append操作可能会导致底层数组重新分配，从而不再共享内存
+	s2 := append(s, 4)
+	s2[1] = 200
+	fmt.Println("s after append:", s)
+	fmt.Println("s1 after append:", s1)
+	fmt.Println("s2 after append:", s2)
+
+	// s和s1仍然共享内存
+	s1[2] = 300
+	fmt.Println("s after modifying s1:", s)
+	fmt.Println("s1 after modifying s1:", s1)
+	fmt.Println("s2 remains unchanged:", s2)
+}
+
+// 切片截断和append
+func SliceTruncateAndAppend() {
+	// 数组
+	var arr [4]int
+	fmt.Printf("brr type: %T\n", arr)
+
+	// 通过[a:b]，会返回切片类型,cap=len(arr)-a,容量从a开始计算
+	// brr切片的底层数组是arr，与arr共享内存
+	brr := arr[1:2]
+	fmt.Printf("brr type: %T\n", brr)
+	fmt.Printf("brr len:%d,brr cap:%d\n", len(brr), cap(brr))
+	// 修改brr会影响arr
+	brr[0] = 100
+	fmt.Println("arr:", arr)
+	fmt.Println("brr:", brr)
+
+	// append只能用于切片类型，不能用于数组类型
+	// 扩容之前对brr进行append操作，会修改arr的值
+	brr = append(brr, 200)
+	fmt.Println("arr after append:", arr)
+	fmt.Println("brr after append:", brr)
+
+	// 对arr修改，会影响brr
+	arr[2] = 300
+	fmt.Println("arr after modifying arr:", arr)
+	fmt.Println("brr after modifying arr:", brr)
+
+	// 对arr修改，但是超出brr的cap范围，不会影响brr
+	arr[3] = 400
+	fmt.Println("arr after modifying arr:", arr)
+	fmt.Println("brr after modifying arr:", brr)
+
+	// 对brr进行append操作，会修改arr，导致对应位置的值被覆盖
+	brr = append(brr, 500)
+	fmt.Println("arr after second append:", arr)
+	fmt.Println("brr after second append:", brr)
+
+	// 继续对brr进行append操作，超过cap范围，brr会重新分配内存，不再与arr共享内存
+	brr = append(brr, 600)
+	fmt.Println("arr after third append:", arr)
+	fmt.Println("brr after third append:", brr)
+
+	fmt.Printf("brr len:%d,brr cap:%d\n", len(brr), cap(brr))
+}
+
+func SlicesFunction() {
+	// 切片的一些函数
+	arr := []int{1, 2, 3, 4, 5}
+	fmt.Println("包含", slices.Contains(arr, 5))
+	fmt.Println("最大者", slices.Max(arr))
+	fmt.Println("最小者", slices.Min(arr))
+
+	brr := make([]int, 3, 5)
+	copy(brr, arr)
+	fmt.Println("brr:", brr)
+
+	type User struct {
+		Name   string
+		Age    int
+		Height float64
+		Weight float64
+	}
+	UserSlice := []User{
+		{"a", 25, 163, 96},
+		{"b", 18, 163, 120},
+		{"c", 25, 168, 110},
+		{"d", 18, 168, 97},
+		{"e", 25, 168, 98},
+		{"f", 18, 168, 99},
+	}
+
+	for _, v := range UserSlice {
+		fmt.Println(v)
+	}
+	// 按照年龄升序，身高升序，体重降序
+	slices.SortFunc(UserSlice, func(a, b User) int {
+		if a.Age != b.Age {
+			return a.Age - b.Age
+		}
+		if a.Height != b.Height {
+			if a.Height > b.Height {
+				return 1
+			} else {
+				return -1
+			}
+		}
+		if a.Weight != b.Weight {
+			if a.Weight > b.Weight {
+				return -1
+			} else {
+				return 1
+			}
+		}
+		return 0
+	})
+	for _, v := range UserSlice {
+		fmt.Println(v)
+	}
+
+}
+
+func main25() {
 	// SliceInit()
-	Expansion()
+	// Expansion()
+	// MemoryShare()
+	// SliceTruncateAndAppend()
+	SlicesFunction()
+
 }
---
+++ basic/字符串.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	s1, s2, s3, s4 := "1", "2", "3", "4"
+	merged := strings.Join([]string{s1, s2, s3, s4}, "-")
+	fmt.Println(merged)
+
+	builder := strings.Builder{}
+	builder.WriteString(s1)
+	builder.WriteString(",")
+	builder.WriteString(s2)
+	builder.WriteString(",")
+	builder.WriteString(s3)
+	builder.WriteString(",")
+	builder.WriteString(s4)
+	fmt.Println(builder.String())
+
+	str := strings.Split(merged, "-")
+	fmt.Println(str)
+
+	str1 := strconv.Itoa(1)
+	fmt.Println(str1)
+	// 字符串转整数
+	i, err1 := strconv.Atoi(str[1])
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	fmt.Println(i)
+
+	// string转int64
+	i64, err := strconv.ParseInt(str[2], 10, 64)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(i64)
+
+}
