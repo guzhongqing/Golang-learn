4756caa82b0576703c9229e8bfdb9b17ec564d42
zqgu5 <zqgu5@iflytek.com>
2025-12-25 22:44:01 +0800

    feat: 添加gRPC客户端和服务器的拦截器功能

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -4,26 +4,74 @@ import (
 	"context"
 	"fmt"
 	grpc_service "golang_learn/go_basic/grpc/idl/service"
+	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+func timer(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	fmt.Printf("timer调用前\n")
+
+	// 记录开始时间
+	start := time.Now()
+
+	// 调用原始方法
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	fmt.Printf("花费时间: %v ms\n", time.Since(start).Milliseconds())
+	fmt.Printf("timer调用后\n")
+
+	return err
+}
+
+func conter(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	fmt.Printf("conter调用前\n")
+
+	// 调用原始方法
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	fmt.Printf("method: %s\n", method)
+	fmt.Printf("conter调用后\n")
+
+	return err
+}
+
 func main() {
-	conn, err := grpc.NewClient("127.0.0.1:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// 全局设置opts ...DialOption
+	conn, err := grpc.NewClient("127.0.0.1:8080", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(
+			grpc.MaxCallRecvMsgSize(1024),
+			grpc.MaxCallSendMsgSize(1024),
+		),
+		// 客户端拦截器
+		// grpc.WithUnaryInterceptor(timer),
+		// 多个拦截器，链式调用，按顺序执行
+		grpc.WithChainUnaryInterceptor(timer, conter),
+	)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
-	// 创建 StudentClient 客户端
-	client := grpc_service.NewStudentClient(conn)
-	// 调用 QueryStudent 方法
-	req := &grpc_service.QueryStudentRequest{
-		Id: 123,
+	const N = 1
+	wg := sync.WaitGroup{}
+	wg.Add(N)
+	for i := 0; i < N; i++ {
+		go func() {
+			defer wg.Done()
+			// 创建 StudentClient 客户端
+			client := grpc_service.NewStudentClient(conn)
+			// 调用 QueryStudent 方法
+			req := grpc_service.QueryStudentRequest{
+				Id: 123,
+			}
+			// 单次调用设置接收消息大小为 1024 字节
+			resp, err := client.QueryStudent(context.Background(), &req, grpc.MaxCallRecvMsgSize(1024))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("response%d: %+v\n", i, resp)
+		}()
 	}
-	resp, err := client.QueryStudent(context.Background(), req)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("response: %+v\n", resp)
+	wg.Wait()
+
 }
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	grpc_service "golang_learn/go_basic/grpc/idl/service"
 	"net"
+	"time"
 
 	grpc "google.golang.org/grpc"
 )
+
+func timer(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+	fmt.Printf("timer调用前\n")
+	// 记录开始时间
+	start := time.Now()
+
+	// 调用原始方法
+	resp, err = handler(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("花费时间: %v ms\n", time.Since(start).Milliseconds())
+	fmt.Printf("timer调用后\n")
+	return resp, nil
+
+}
+
+func conter(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp any, err error) {
+	// 调用原始方法前，打印方法名
+	fmt.Printf("conter调用前\n")
+	fmt.Printf("method: %s\n", info.FullMethod)
+
+	// 调用原始方法
+	resp, err = handler(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	// 调用原始方法后，打印方法名
+	fmt.Printf("conter调用后\n")
+	return resp, nil
+
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "127.0.0.1:8080")
@@ -15,7 +49,11 @@ func main() {
 	}
 	// 监听的地址
 	fmt.Printf("server is running on %s\n", listener.Addr().String())
-	server := grpc.NewServer()
+	// 全局设置opts ...ServerOption
+	// server := grpc.NewServer(grpc.UnaryInterceptor(timer))
+	// 链式调用
+	server := grpc.NewServer(grpc.ChainUnaryInterceptor(timer, conter))
+
 	// 注册 StudentServer 服务，可以注册多个service
 	grpc_service.RegisterStudentServer(server, Student{})
 	server.Serve(listener)
