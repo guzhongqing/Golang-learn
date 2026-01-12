70c4cd29f487a4e627bd48a778d5266fa0f7a8a4
zqgu5 <zqgu5@iflytek.com>
2026-01-12 20:59:41 +0800

    fix: 修改slog入参错误

--- go_frame/post_news/main.go
+++ go_frame/post_news/main.go
@@ -14,7 +14,7 @@ func main() {
 	// 初始化数据库连接,相关路径是相对于main.go的路径
 	// 2. 初始化数据库连接（带错误捕获）
 	if err := gorm.CreateConnection(viperConfig); err != nil {
-		slog.Error("数据库初始化失败", err.Error()) // 失败直接退出，提示明确错误
+		slog.Error("数据库初始化失败", "err", err)
 	}
 
 }
